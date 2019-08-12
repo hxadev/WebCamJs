6f81e4e770b06e76f1876368933cee4e33b683b3
posihx <alfonso_h_x@hotmail.com>
2019-08-12 18:45:06 -0500

    Upload Changes

--- controller.js
+++ controller.js
@@ -2,15 +2,19 @@
 
 const video=document.getElementById('video');
 const canvas=document.getElementById('canvas');
-const snap=document.getElementById('snap');
+const btnShot=document.getElementById('shot');
 const errorMsgElement=document.querySelector('span#errorMsg');
+const btnSaveImage=document.getElementById("saveImage");
+var image="";
 
-const constraints={audio:true,video:{width:640, height:480}};
+const constraints={audio:false,video:{width:200, height:200}};
 
 async function init(){
     try{
         const stream=await navigator.mediaDevices.getUserMedia(constraints);
         handleSuccess(stream);
+        console.log(navigator.mediaDevices.enumerateDevices());
+
     }catch(ex)
     {
         errorMsgElement.innerHTML='navigator.getUserMedia error:${ex.toString()}';
@@ -22,9 +26,22 @@ function handleSuccess(stream){
     video.srcObject=stream;
 }
 
+
 init();
 
 var context=canvas.getContext('2d');
-snap.addEventListener("click",function(){
-    context.drawImage(video,0,0,640,480 );
+btnShot.addEventListener("click",function(){
+    context.drawImage(video,0,0,300,200 );
+    
 });
+
+
+
+function downloadImage(context){
+    image=canvas.toDataURL("image/jpg");
+    console.log(context);
+    context.href=image;
+}
+
+
+
